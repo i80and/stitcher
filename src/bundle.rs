d35684aebe5af6726b64d6e67871ab8039fa9077
Heli Aldridge <heli@heli.pet>
2024-12-20 11:39:49 -0500

    Initial commit

---
+++ src/bundle.rs
@@ -0,0 +1,128 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::{Path, PathBuf};
+
+use anyhow::{Context, Result};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct SiteMetadata {
+    project: String,
+    branch: String,
+}
+
+impl SiteMetadata {
+    pub fn get_namespace(&self) -> String {
+        format!("{}/{}", self.project, self.branch)
+    }
+}
+
+pub struct BundleIntoIterator<'a> {
+    bundle: &'a mut Bundle,
+    index: usize,
+}
+
+pub struct BundleElement {
+    pub name: PathBuf,
+    pub data: BundleElementData,
+}
+
+impl BundleElement {
+    pub fn new(name: PathBuf, data: BundleElementData) -> Self {
+        Self { name, data }
+    }
+}
+
+pub enum BundleElementData {
+    Document(bson::Bson),
+    Asset(Vec<u8>),
+}
+
+pub struct Bundle {
+    pub metadata: SiteMetadata,
+    archive: zip::ZipArchive<BufReader<File>>,
+}
+
+impl<'a> IntoIterator for &'a mut Bundle {
+    type Item = anyhow::Result<BundleElement>;
+    type IntoIter = BundleIntoIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        BundleIntoIterator {
+            bundle: self,
+            index: 0,
+        }
+    }
+}
+
+impl Bundle {
+    pub fn open(path: impl AsRef<Path>) -> Result<Self> {
+        let file = std::fs::File::open(path).unwrap();
+        let reader = std::io::BufReader::new(file);
+        let mut archive = zip::ZipArchive::new(reader).unwrap();
+
+        let metadata = bson::from_reader(archive.by_name("site.bson")?)?;
+
+        Ok(Bundle { metadata, archive })
+    }
+}
+
+impl<'a> Iterator for BundleIntoIterator<'a> {
+    type Item = anyhow::Result<BundleElement>;
+
+    fn next(&mut self) -> Option<anyhow::Result<BundleElement>> {
+        loop {
+            let idx = self.index;
+
+            if idx >= self.bundle.archive.len() {
+                return None;
+            }
+
+            self.index += 1;
+
+            let mut file = self.bundle.archive.by_index(idx).unwrap();
+            let filename = match file.enclosed_name() {
+                Some(path) => path,
+                None => {
+                    eprintln!("Bundle entry {} has a prohibited path", file.name());
+                    continue;
+                }
+            };
+
+            if !file.is_file() {
+                continue;
+            }
+
+            if filename.starts_with("documents") {
+                return Some(
+                    bson::from_reader(file)
+                        .with_context(|| {
+                            format!("Error deserializing BSON: {}", filename.display())
+                        })
+                        .map(|value| {
+                            BundleElement::new(filename, BundleElementData::Document(value))
+                        }),
+                );
+            } else if filename.starts_with("assets") {
+                let mut buf: Vec<u8> = vec![];
+                if let Err(err) = file
+                    .read_to_end(&mut buf)
+                    .with_context(|| format!("Error reading asset: {}", filename.display()))
+                {
+                    return Some(Err(err));
+                }
+
+                return Some(Ok(BundleElement::new(
+                    filename,
+                    BundleElementData::Asset(buf),
+                )));
+            } else if filename.starts_with("diagnostics") {
+                continue;
+            } else if filename == Path::new("site.bson") {
+                continue;
+            } else {
+                eprintln!("Unexpected bundle entry: {}", filename.display());
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+
+mod bundle;
+
+#[derive(clap::Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Bundles to operate on
+    bundles: Vec<PathBuf>,
+
+    /// The path to which to save the stitched bundle
+    #[arg(short, long, value_name = "FILE")]
+    output: PathBuf,
+}
+
+fn splice(
+    bundles: &mut [bundle::Bundle],
+    mut out_bundle: zip::ZipWriter<BufWriter<File>>,
+) -> Result<()> {
+    let options =
+        zip::write::SimpleFileOptions::default().compression_method(zip::CompressionMethod::Stored);
+
+    let (tx, rx) = crossbeam_channel::bounded(10);
+
+    let n_cpus = std::thread::available_parallelism()?.get();
+    assert!(n_cpus >= 1_usize);
+    let mut pool = scoped_threadpool::Pool::new(u32::try_from(n_cpus)?);
+
+    std::thread::spawn(move || loop {
+        let packet = rx.recv().unwrap();
+
+        match packet {
+            Some(name) => out_bundle.start_file(name, options).unwrap(),
+            None => {
+                out_bundle.finish().unwrap();
+                return;
+            }
+        }
+    });
+
+    pool.scoped(|scope| {
+        // Create references to each element in the vector ...
+        for bundle in bundles {
+            scope.execute(|| {
+                let bundle_ns = bundle.metadata.get_namespace();
+                for entry in bundle {
+                    let entry = entry.unwrap();
+                    let name = entry
+                        .name
+                        .to_str()
+                        .ok_or_else(|| {
+                            anyhow::anyhow!(
+                                "Failed to convert entry name to string: {:?}",
+                                entry.name
+                            )
+                        })
+                        .unwrap();
+                    let name = format!("{}/{}", bundle_ns, name);
+                    tx.send(Some(name)).unwrap();
+                }
+            });
+        }
+    });
+
+    tx.send(None).unwrap();
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let output_file = File::create(cli.output)?;
+    let output_writer = BufWriter::new(output_file);
+    let output_archive = zip::ZipWriter::new(output_writer);
+
+    let mut bundles = vec![];
+    for path in cli.bundles {
+        let bundle = bundle::Bundle::open(&path)?;
+        bundles.push(bundle);
+    }
+
+    splice(&mut bundles, output_archive)?;
+
+    Ok(())
+}
